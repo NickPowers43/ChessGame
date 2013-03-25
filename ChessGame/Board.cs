64718fa59b168656a444fe3991f11019f6750eb5
Nick Powers <nickpowers43@gmail.com>
2013-03-24 22:31:02 -0400

    Added some classes

---
+++ ChessGame/Board.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+using OpenTK;
+using OpenTK.Graphics;
+using OpenTK.Graphics.OpenGL;
+
+namespace ChessGame
+{
+    class Board
+    {
+        const int SIZE = 8;
+
+        private Piece[,] pieces = new Piece[SIZE, SIZE];
+
+        public Board()
+        {
+
+        }
+    }
+}
---
+++ ChessGame/Game.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+using OpenTK;
+using OpenTK.Graphics;
+using OpenTK.Graphics.OpenGL;
+
+namespace ChessGame
+{
+    class Game
+    {
+        private Board board;
+
+        public Game()
+        {
+
+        }
+    }
+}
