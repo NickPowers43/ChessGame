f72ad0201a98931f2ecbfadac5377953ce1bd3f5
Michael Corallo <mcorallo@students.kennesaw.edu>
2013-04-09 11:20:20 -0400

    initial upload

--- ChessGame/Piece.cs
+++ ChessGame/Piece.cs
@@ -9,8 +9,22 @@ using OpenTK.Graphics.OpenGL;
 
 namespace ChessGame
 {
-    class Piece
+    abstract class Piece
     {
+        const int WHITE = 1;
+        const int BLACK = 2;
 
+        public Piece(int color)
+        {
+
+
+        }
+
+        //move logic for the extended piece
+        public void move();
+
+        //check to make sure the move is legal
+        public bool isLegal();
+       
     }
 }
---
+++ ChessGame/Rook.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace ChessGame
+{
+    class Rook : Piece
+    {
+        public Rook(int color)
+        {
+            
+        }
+
+    }
+}
